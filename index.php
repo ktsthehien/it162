e123e164e4d9d9ad8d1a5c669b79a5253874f2c4
Hien Nguyen <72104238+ktsthehien@users.noreply.github.com>
2021-04-19 22:47:02 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="My Photo" />
+ <img src="images/tablet.jpg" class="tablet" alt="My Photo" />
+ <img src="images/phone.jpg" class="phone" alt="My Photo" />
+ 
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>A little about me!</h3>
+ 
+ <p>I am a web developer who is passionate about solving website challenges. With dynamic visuals, rational design skill, and experiential thinking, I focus on both artistic value and user’s needs in every design decision.</p>
+ <p>Some of my favorites things are playing soccer, traveling, reading books, and watching movies.  </p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
+
